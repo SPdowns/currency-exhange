1f7cf6a27f0eef3825da61dd582a049e2692c99f
Sean Downs <spdowns@protonmail.com>
2020-07-03 15:19:14 -0700

    fix syntax and spelling in main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { CurrencyService } from './currency-service.js'
-import 
+import { ExchangeFormula } from './exchange-formula.js'
 
 $(document).ready(function () {
   $("form").submit(function() {
@@ -15,8 +15,10 @@ $(document).ready(function () {
       const response = await currencyService.getExchagneRate();
       getElements(response)
     })();
-    function getElements(responseParam) {
+    function getElements(responseParam, countryInput) {
       if (countryInput === "AUD") {
-
-        $("#output-section").text
+        $("#output-section").text(responseParam)
       }
+    }
+  });
+});
