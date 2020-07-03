7aaf9f70ff921b823110db3f2ae2e4b97037be03
Sean Downs <spdowns@protonmail.com>
2020-07-03 08:51:39 -0700

    add a little html and starts building the main.js api call

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function () {
-  
+  $("form").submit(function() {
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+  });
 });
