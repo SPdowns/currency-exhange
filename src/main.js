167ea9547c028c4ec89e5c5005889f08444caf3e
Sean Downs <spdowns@protonmail.com>
2020-07-03 15:16:14 -0700

    add input amount to html and main.js. changes business.js to exchangeformula.js.

--- src/main.js
+++ src/main.js
@@ -3,25 +3,20 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { CurrencyService } from './currency-service.js'
+import 
 
 $(document).ready(function () {
   $("form").submit(function() {
     event.preventDefault();
+    let countryInput = $("#input-country").val()
+    let amountInput = $("#input-amount").val()
     (async () => {
       let currencyService = new CurrencyService();
       const response = await currencyService.getExchagneRate();
       getElements(response)
     })();
     function getElements(responseParam) {
-      let countryInput = $("#country-selector").val();
-      let arrayCountry = responseParam.conversion_rates
-      arrayCountry.map()
-      if (countryInput === arrayCountry.key) {
-      $("#output-section").append(key + " : " + vaule + `<br>`);
-        });
-      } else {
-        $("#output-section").text('In space, no one can hear your response');
+      if (countryInput === "AUD") {
+
+        $("#output-section").text
       }
-    }
-  });
-});
