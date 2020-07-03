87daeab68683175662e005c56d4dfefff20ff3a2
Sean Downs <spdowns@protonmail.com>
2020-07-03 15:39:39 -0700

    add four additional currency and convert them. next is dom

--- src/main.js
+++ src/main.js
@@ -3,21 +3,34 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { CurrencyService } from './currency-service.js'
-import { ExchangeFormula } from './exchange-formula.js'
+//import { ExchangeFormula } from './exchange-formula.js'
 
 $(document).ready(function () {
   $("form").submit(function() {
     event.preventDefault();
-    let countryInput = $("#input-country").val()
-    let amountInput = $("#input-amount").val()
+    let countryInput = $("#input-country").val();
+    let amountInput = $("#input-amount").val();
+
     (async () => {
       let currencyService = new CurrencyService();
       const response = await currencyService.getExchagneRate();
-      getElements(response)
+      getElements(response, countryInput, amountInput)
     })();
-    function getElements(responseParam, countryInput) {
+    
+    function getElements(responseParam, countryInput, amountInput) {
+      console.log(responseParam, countryInput, amountInput)
       if (countryInput === "AUD") {
-        $("#output-section").text(responseParam)
+        $("#output-section").text(responseParam.conversion_rates.AUD * amountInput)
+      } else if (countryInput === "GBP") {
+        $("#output-section").text(responseParam.conversion_rates.GBP * amountInput)
+      } else if (countryInput === "KRW") {
+        $("#output-section").text(responseParam.conversion_rates.KRW * amountInput)
+      } else if (countryInput === "MXN") {
+        $("#output-section").text(responseParam.conversion_rates.MXN * amountInput)
+      } else if (countryInput === "HKD") {
+        $("#output-section").text(responseParam.conversion_rates.GBP * amountInput)
+      } else {
+        $("#output-section").text("This ain gon work")
       }
     }
   });
