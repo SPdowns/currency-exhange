38384af6fc0168b9a77b7fcfc2c6ba84b2353fcd
Sean Downs <spdowns@protonmail.com>
2020-07-03 11:07:04 -0700

    will show im stuck on passing in a country code. let country is defined and taking in the correct vallue, when attched to end of responseParam, returns undefined

--- src/main.js
+++ src/main.js
@@ -7,17 +7,16 @@ import { CurrencyService } from './currency-service.js'
 $(document).ready(function () {
   $("form").submit(function() {
     event.preventDefault();
-    
+    let country = $("#country-selector").val();
     (async () => {
       let currencyService = new CurrencyService();
-      const response = await currencyService.getExchagneRate();
+      const response = await currencyService.getExchagneRate(country);
       getElements(response)
     })();
-    
     function getElements(responseParam) {
-      console.log(responseParam.conversion_rates.ARS)
+      console.log(country, responseParam.conversion_rates.AUD)
       if (responseParam.conversion_rates) {
-        $("#output-section").append(responseParam.conversion_rates.ARS)
+        $("#output-section").append(responseParam.conversion_rates)
       } else {
         $("#output-section").text('In space, no one can hear your response');
       }
